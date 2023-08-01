fbfd9f2fc011ef1e605a18b578e42a1f90a89bb1
Adrienne Williams <82108228+adriennewilliamsyext@users.noreply.github.com>
2023-08-01 11:23:31 -0400

    Update consts.ts- remove keys

--- src/common/consts.ts
+++ src/common/consts.ts
@@ -1,8 +1,8 @@
 
 //Replace with Your Yext Business ID
-export const businessId = 3220514;
+export const businessId = YOUR_BUSINESS_ID;
 //Replace with Your Search Experience API Key
-export const apiKey = "8a86944f50d0cbf291435e78c90b9117";
+export const apiKey = "YOUR_API_KEY";
 //Replace with Your Search Experience experience key
 export const experienceKey = "commerce-search-starter";
 //Replace with Your Search Experience locale
